028c4ce3ab59a7e97daacbb7aa1467cf4ca46428
sbiswal <sbiswal@iastate.edu>
2020-09-07 20:21:23 -0500

    WipeTab Files

---
+++ dist/precache-manifest.fcde66cfdd346b8bad0f7bf9e22d7fee.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c659d9d9d0acd39fbebf7a5cd87e3e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5b4e831d6f934a890e7",
+    "url": "/static/css/2.867655b0.chunk.css"
+  },
+  {
+    "revision": "ad5debc336ef5c137de7",
+    "url": "/static/css/main.e9b058ce.chunk.css"
+  },
+  {
+    "revision": "a5b4e831d6f934a890e7",
+    "url": "/static/js/2.18f28baa.chunk.js"
+  },
+  {
+    "revision": "36216d59375f2701be1fca153a4309f6",
+    "url": "/static/js/2.18f28baa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad5debc336ef5c137de7",
+    "url": "/static/js/main.bd88f471.chunk.js"
+  },
+  {
+    "revision": "414c8648b5186f341b71",
+    "url": "/static/js/runtime-main.74cf793d.js"
+  },
+  {
+    "revision": "c70a5bee7e2f64f5b734313e7937af85",
+    "url": "/static/media/close.c70a5bee.svg"
+  },
+  {
+    "revision": "0876018e82b4da86bbe8d9a23a5f4efe",
+    "url": "/static/media/flexmonster-icons.0876018e.woff"
+  },
+  {
+    "revision": "75437a3be42130e2939db5c7d68273ee",
+    "url": "/static/media/flexmonster-icons.75437a3b.svg"
+  },
+  {
+    "revision": "f421247f95d3f306232bd5fd73ee6b8e",
+    "url": "/static/media/flexmonster-icons.f421247f.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
